67249646f16b460ecb642b668b1c61a4806e1d46
yanwari <yanwari.asd@gmail.com>
2026-04-24 13:27:23 +0700

    capekkk

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ def create_app():
         api_secret=app.config['CLOUDINARY_API_SECRET']
     )
 
+    with app.app_context():
+        db.create_all()  # ← tambah ini
+
     from models.artikel import User
 
     @login_manager.user_loader
@@ -34,4 +37,6 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
